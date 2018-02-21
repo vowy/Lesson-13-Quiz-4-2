f49a80a91807fa192b0cecbe54af2087a8bbdf6b
vowy <e.b.parsons@gmail.com>
2018-02-21 16:53:22 -0600

    Quiz 4-2 "99 Bottles of Juice on the Wall" JS

---
+++ 99Bottles.js
@@ -0,0 +1,14 @@
+
+ var num = 99;
+
+ while (num >= 0) {
+ console.log
+  (num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... ");
+ console.log
+  ((num-1) + " bottles of juice on the wall!");
+  if (num = 1){
+    console.log
+     (num + " bottle of juice on the wall! " + num + " bottle of juice! Take one down, pass it around... ");
+    console.log
+     ((num-1) + " bottles of juice on the wall!");
+  num = num - 1;}
